ff07887d9eca1efff9deb0d02959c31fafea04e3
ReubenKimClemens <reuben.kim.clemens@gmail.com>
2025-02-18 19:50:37 -0500

    Initial Commit

---
+++ idm362-reubenC/idm362_reubenCApp.swift
@@ -0,0 +1,17 @@
+//
+//  idm362_reubenCApp.swift
+//  idm362-reubenC
+//
+//  Created by Reuben Clemens on 2/18/25.
+//
+
+import SwiftUI
+
+@main
+struct idm362_reubenCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
