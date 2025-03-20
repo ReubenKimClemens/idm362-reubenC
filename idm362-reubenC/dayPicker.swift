d9902faa2fb17ef1fee0bc1edc161206b44ac6db
ReubenKimClemens <reuben.kim.clemens@gmail.com>
2025-03-20 15:50:46 -0400

    commit update

--- idm362-reubenC/dayPicker.swift
+++ idm362-reubenC/dayPicker.swift
@@ -8,8 +8,14 @@
 import SwiftUI
 
 struct dayPicker: View {
+    
+    @State var isOn = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Toggle(isOn: $isOn) {
+            Text("I'm not a robot")
+        }
+
     }
 }
 
